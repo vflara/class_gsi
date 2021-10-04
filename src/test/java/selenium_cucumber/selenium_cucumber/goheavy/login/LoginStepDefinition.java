89b1eacccf8a15480b8c8653755929d5f4020d9d
gsi-joseespinosa <51973444+gsi-joseespinosa@users.noreply.github.com>
2021-10-03 23:25:28 -0400

    initialize goheavy

---
+++ src/test/java/selenium_cucumber/selenium_cucumber/goheavy/login/LoginStepDefinition.java
@@ -0,0 +1,35 @@
+package selenium_cucumber.selenium_cucumber.goheavy.login;
+
+import io.cucumber.java.en.*;
+
+public class LoginStepDefinition {
+
+	public LoginStepDefinition() {
+
+	}
+
+	@Given("The unauthenticated GoHeavy User is in the Login view")
+	public void the_unauthenticated_go_heavy_user_is_in_the_view(String string) {
+
+	}
+
+	@When("User insert email {string} and password {string1}")
+	public void user_insert_email_and_password(String string, String string1) {
+
+	}
+
+	@When("User clicks on the {string} button")
+	public void user_clicks_on_the_button(String string) {
+
+	}
+
+	@Then("The system allows the user access to the system")
+	public void the_system_allows_the_user_access_to_the_system() {
+
+	}
+
+	@Then("Sytem redirects to {string} view")
+	public void sytem_redirects_to_dashboard_view(String string) {
+
+	}
+}
---
+++ src/test/resources/features/login/login.feature
@@ -0,0 +1,14 @@
+Feature: Test
+
+  Scenario Outline: Allowing and redirecting user according to its access
+    Given The unauthenticated GoHeavy User is in the Login view
+    When User insert email <emails> and password <passwords>
+    And User clicks on the "Sign in" button
+    Then The system allows the user access to the system
+    And Sytem redirects to <views> view
+
+    Examples: 
+      | emails             | passwords | views             |
+      | novanick@gmail.com | @User123  | Dashboard         |
+      | evakings@gmail.com | @User123  | Drivers List      |
+      | piper95@gmail.com  | @User123  | Fleet Owners List |
